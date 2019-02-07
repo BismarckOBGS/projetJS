0f25c46b7f89628d1615c19203f745b8a7262120
BISMARCK-OBGS <okoubisso.guyselassie@gmail.com>
2019-02-07 18:12:25 +0000

    first commit

---
+++ js/map.js
@@ -0,0 +1,75 @@
+
+//Initialisation de la carte
+
+
+var map = L.map('map').setView([45.743317, 4.815747], 13);
+L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+ { attribution: 'Map data &copy; <a href="http://www.osm.org">OpenStreetMap</a>' })
+ 
+ .addTo(map);
+
+ 
+
+ // Chargement des données depui JCDecaux
+
+
+var url = 'https://api.jcdecaux.com/vls/v1/stations?contract=Lyon&apiKey=132e27d3cf194a30390caf93dfad62b69949fe27';
+var oXhr = new XMLHttpRequest();
+
+oXhr.onload = function () {
+  var data = JSON.parse(this.responseText);
+  // ici les données sont exploitables
+  var markers = new Array();
+
+  for(var i=0;i<data.length;i++)
+  {
+    var staVel = data[i];
+    var posX = staVel.position.lat;
+    var posY = staVel.position.lng
+
+    var initDispo =  staVel.available_bike_stands;
+    var dispo =  staVel.available_bikes;
+    var status = staVel.status;
+
+    var marker = L.marker([posX, posY]).addTo(map);
+    
+    marker.bindPopup(staVel.status);
+
+
+    marker.on('mouseover', function(e){ marker.openPopup(); });
+    marker.on('mouseout', function(e){ marker.closePopup(); });
+
+  }
+
+};
+
+oXhr.onerror = function (data) {
+  console.log('Erreur ...');
+};
+oXhr.open('GET', url, true);
+oXhr.send(null);
+
+
+
+
+//pour le slider
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var jumbotrons = document.getElementsByClassName("jumbotron");
+  for (i = 0; i < jumbotrons.length; i++) {
+    jumbotrons[i].style.display = "none"; 
+  }
+  slideIndex++;
+  if (slideIndex > jumbotrons.length) {slideIndex = 1} 
+  jumbotrons[slideIndex-1].style.display = "block"; 
+  setTimeout(showSlides, 1500); // Change image every 2 seconds
+}
+
+
+
+
+
