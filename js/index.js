e83f6d927c0517aca5fb9a60296eae98b7dd5b9a
daviidy <yaodavidarmel@gmail.com>
2019-02-21 18:57:05 +0000

    message

--- js/index.js
+++ js/index.js
@@ -74,5 +74,5 @@ function showSlides() {
   slideIndex++;
   if (slideIndex > jumbotrons.length) {slideIndex = 1}
   jumbotrons[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 1000); // Change image every 2 seconds
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
